159abc9051e403aa89bedc7400bfecb8c8043bef
Scappy <info.scappy@gmail.com>
2022-05-05 21:50:28 +0200

    Update Image-Cropper and Storage

--- app/src/main/java/com/example/messenger/LoginActivity.java
+++ app/src/main/java/com/example/messenger/LoginActivity.java
@@ -1,8 +1,5 @@
 package com.example.messenger;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,11 +10,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity
 {
@@ -56,6 +55,15 @@ public class LoginActivity extends AppCompatActivity
             }
         });
 
+        PhoneLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                   Intent phoneLoginIntent = new Intent(LoginActivity.this, PhoneLoginActivity.class);
+                   startActivity(phoneLoginIntent);
+            }
+        });
+
     }
 
 
@@ -79,6 +87,8 @@ public class LoginActivity extends AppCompatActivity
              loadingBar.setMessage("Please wait.....");
              loadingBar.setCanceledOnTouchOutside(true);
              loadingBar.show();
+
+
              mAuth.signInWithEmailAndPassword(email, password)
                      .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                          @Override
--- app/src/main/java/com/example/messenger/SettingsActivity.java
+++ app/src/main/java/com/example/messenger/SettingsActivity.java
@@ -1,15 +1,20 @@
 package com.example.messenger;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -19,6 +24,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.HashMap;
 
@@ -29,10 +37,14 @@ public class SettingsActivity extends AppCompatActivity
     private Button UpdateAccountSetting;
     private EditText userName, userStatus;
     private CircleImageView userProfileImage;
+    ActivityResultLauncher<String> mGetContent;
 
     private String currentUserID;
     private FirebaseAuth mAuth;
     private DatabaseReference RootRef;
+
+    private static final int GalleryPick =1;
+    private StorageReference UserProfileImageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -43,6 +55,7 @@ public class SettingsActivity extends AppCompatActivity
         mAuth = FirebaseAuth.getInstance();
         currentUserID = mAuth.getCurrentUser().getUid();
         RootRef = FirebaseDatabase.getInstance().getReference();
+        UserProfileImageRef = FirebaseStorage.getInstance().getReference().child("Profile Images");
 
 
         InitializeFields();
@@ -60,6 +73,11 @@ public class SettingsActivity extends AppCompatActivity
         });
 
         RetrieveUserInfo();
+
+
+
+
+
     }
 
 
@@ -69,7 +87,60 @@ public class SettingsActivity extends AppCompatActivity
         userName = (EditText) findViewById(R.id.set_user_name);
         userStatus = (EditText) findViewById(R.id.set_profile_status);
         userProfileImage = (CircleImageView) findViewById(R.id.set_profile_image);
+
+        userProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                mGetContent.launch("image/*");
+            }
+        });
+
+        mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result)
+            {
+                Intent intent = new Intent(SettingsActivity.this, CropperActivity.class);
+                intent.putExtra("DATA", result.toString());
+                startActivityForResult(intent, 101);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode==-1 && requestCode==101)
+        {
+            String result=data.getStringExtra("RESULT");
+            Uri resultUri=null;
+            if (result!=null)
+            {
+                resultUri=Uri.parse(result);
+
+                // Test Storeage
+                StorageReference filePath = UserProfileImageRef.child(currentUserID + ".jpg");
+
+                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) 
+                    {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(SettingsActivity.this, "Profile Image Uploaded Successfully", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            String message = task.getException().toString();
+                            Toast.makeText(SettingsActivity.this, "Error" + message, Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+            }
+        }
+    }
+
 
 
     private void UpdateSettings()
